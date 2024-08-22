1daaeb8c0177b799fb7a2ca44c7ac7e0dc21493c
sashka199336 <160388443+sashka199336@users.noreply.github.com>
2024-08-22 23:13:24 +1000

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,62 +1,53 @@
 class Animal:
-    def __init__(self,name):
-     self.alive = True
-     self.fed = False
-     self.name= name
+    def __init__(self, name):
+        self.alive = True
+        self.fed = False
+        self.name = name
+
 
     def eat(self, food):
-        self.food = food
-        if food in False:
-            print(f'{self.name} съел {food.name}')
-            return True
+
+
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+
         else:
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
 
 
-class  Plant:
-    class Plant:
-        def __init__(self, name):
-            self.edible = False
-            self.name = name
+
+
+class Plant:
+    def __init__(self, name):
+        self.edible = False
+        self.name = name
+
+
+
 
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-
-            self.fed = True
-        else:
-             print(f"{self.name} не стал есть {food.name}")
-        self.alive = False
-
+    pass
 
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
+    pass
 
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-        self.alive = False
+
 class Flower(Plant):
-    def __init__(self, name):
-        self.edible = True
-        self.name = name
+    # Заглушка
+    pass
+
 
 class Fruit(Plant):
+
     def __init__(self, name):
         self.edible = True
         self.name = name
 
 
 
-a1 = Predator('Волк с Уолл-Стрит')
-a2 = Mammal('Хатико')
-p1 = Flower('Цветик семицветик')
-p2 = Fruit('Заводной апельсин')
 
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
