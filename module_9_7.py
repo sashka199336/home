39f29a0700c8a0525e6ef405b535af0b310ed7f9
sashka199336 <160388443+sashka199336@users.noreply.github.com>
2024-10-04 17:02:31 +1000

    Update module_9_7.py

--- module_9_7.py
+++ module_9_7.py
@@ -1,10 +1,26 @@
-def all_variants(text):
-    x = 0
-    y = 0
-    for x in range(len(text)):
-        for y in range(x + 1, len(text) + 1):
-            yield text[x:y]
+def is_prime(func):
 
-a = all_variants("abc")
-for i in a:
-    print(i)
+    def wrapper(*args, ** kwargs):
+        result = func(*args, ** kwargs)
+        sum_ = sum(args)
+        k = 0
+        for i in range(2, sum_ // 2 + 1):
+            if sum_ % i == 0:
+                k = k +1
+        if k <= 0:
+            print('Простое')
+        else:
+            print('Составное')
+        return result
+    return wrapper
+
+
+@is_prime
+
+def sum_three(*args):
+    return sum(args)
+
+
+
+result = sum_three(2, 3, 6)
+print(result)
