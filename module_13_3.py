6d45db54de013deda27b8481faba35e41c24a7c1
sashka199336 <160388443+sashka199336@users.noreply.github.com>
2024-10-20 03:42:18 +1000

    Update module_13_3.py

--- module_13_3.py
+++ module_13_3.py
@@ -8,14 +8,14 @@ bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-# Запускается только тогда, когда написана команда '/start' в чате с ботом
+
 @dp.message_handler(commands=['start'])
 async def start_message(message):
     # Ответ бота в чате телеграм
     await message.answer("Привет! Я бот, помогающий твоему здоровью")
 
 
-# Запускается при любом обращении. Не описаном ранее.
+
 @dp.message_handler()
 async def all_message(message):
     await message.answer("Введите команду /start, чтобы начать общение")
