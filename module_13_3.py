6ffa185c69e879e699402c2e6a9b7ed68af4e154
sashka199336 <160388443+sashka199336@users.noreply.github.com>
2024-10-20 03:26:01 +1000

    Create module_13_3.py

---
+++ module_13_3.py
@@ -0,0 +1,24 @@
+rom aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+
+api = '8054112588:AAHvoWrK_8-gw6zlciV9NG7bHSXyKiBrZkU'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+# Запускается только тогда, когда написана команда '/start' в чате с ботом
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    # Ответ бота в чате телеграм
+    await message.answer("Привет! Я бот, помогающий твоему здоровью")
+
+
+# Запускается при любом обращении. Не описаном ранее.
+@dp.message_handler()
+async def all_message(message):
+    await message.answer("Введите команду /start, чтобы начать общение")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
