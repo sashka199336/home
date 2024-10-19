0c7b8815bf153d537e1815b48afdde26337effb0
sashka199336 <160388443+sashka199336@users.noreply.github.com>
2024-10-20 02:46:34 +1000

    Create module_13_2.py

---
+++ module_13_2.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from aiogram.utils import executor
+
+api = '8054112588:AAHvoWrK_8-gw6zlciV9NG7bHSXyKiBrZkU'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await bot.send_message(chat_id=message.from_user.id, text='Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+    await bot.send_message(chat_id=message.from_user.id, text='Введите команду /start, чтобы начать общение.')
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
