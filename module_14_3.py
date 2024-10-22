323df8b91b3c8d449f3bda22eb41374c5cb0a900
sashka199336 <160388443+sashka199336@users.noreply.github.com>
2024-10-23 07:40:31 +1000

    Update module_14_3.py

--- module_14_3.py
+++ module_14_3.py
@@ -1,149 +1,178 @@
 import asyncio
 import logging
-import sys, os
-
-from aiogram import Bot, Dispatcher, F
-from aiogram.filters import Command, CommandStart
-from aiogram.types import Message, CallbackQuery, FSInputFile
-
-from aiogram.fsm.state import StatesGroup, State
+from aiogram import Bot, Dispatcher
+from aiogram.types import FSInputFile, Message, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton
+from aiogram.filters import Command
+from aiogram import Router
 from aiogram.fsm.context import FSMContext
-from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
-
-#
-with open("token.txt") as f:
-    TOKEN = f.read().strip()
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
 
 
-bot = Bot(token=TOKEN)
+API_TOKEN = ''
 
 
-dp = Dispatcher()
+logging.basicConfig(level=logging.INFO)
 
 
-# Состояния
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
+
+
+router = Router()
+
+
 class UserState(StatesGroup):
     age = State()
     growth = State()
     weight = State()
 
 
-# Клавиатуры
-def kb_menu(text: str | list):
-
-    builder = ReplyKeyboardBuilder()
-    if isinstance(text, str):
-        text = [text]
-    [builder.button(text=txt) for txt in text]
-    return builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
-
-
-def ikb_menu(text: str | list):
-
-    builder = InlineKeyboardBuilder()
-    if isinstance(text, str):
-        text = [text]
-    [builder.button(text=txt, callback_data=txt) for txt in text]
-    return builder.as_markup()
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Рассчитать'), KeyboardButton(text='Информация')],
+        [KeyboardButton(text='Купить')]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
 
 
-builder = InlineKeyboardBuilder()
-[builder.button(text=f'Product{i}', callback_data='product_buying') for i in range(1, 5)]
-ikb_buy_menu = builder.as_markup()
+inline_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories'),
+         InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')]
+    ]
+)
 
 
-# Обработчики команд
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-
-    await message.answer("Привет! Я бот помогающий твоему здоровью.",
-                         reply_markup=kb_menu(['Рассчитать', 'Информация', 'Купить']))
-
-
-# Обработчики команд без префикса
-@dp.message(F.text.lower().startswith('рассчитать'))
-async def main_menu(message: Message) -> None:
-
-    await message.answer("Выберите опцию:",
-                         reply_markup=ikb_menu(['Рассчитать норму калорий', 'Формулы расчёта']))
+products_inline_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='CodliverOil', callback_data='product_buying'),
+         InlineKeyboardButton(text='PapayaLeaf', callback_data='product_buying')],
+        [InlineKeyboardButton(text='VitaminCOriginal', callback_data='product_buying'),
+         InlineKeyboardButton(text='CranberryExtract', callback_data='product_buying')],
+        [InlineKeyboardButton(text='Ginseng', callback_data='product_buying'),
+         InlineKeyboardButton(text='SeleniumAdvanced', callback_data='product_buying')]
+    ]
+)
 
 
-@dp.message(F.text.lower().startswith('купить'))
-async def get_buying_list(message: Message) -> None:
-    for i in range(1, 5):
-        await message.answer(f'Название: Product{i} | Описание: описание {i} | Цена: {i * 100}')
-        # await bot.send_photo(message.chat.id, 'product.jpg')
-        await message.answer_photo(FSInputFile('product.webp'))
-    await message.answer('Выберите продукт для покупки:', reply_markup=ikb_buy_menu)
+@router.message(Command(commands=['start']))
+async def start(message: Message):
+    logging.info("Команда /start обработана")
+    await message.answer('Привет! Я бот, помогающий твоему здоровью. Выберите действие:', reply_markup=keyboard)
 
 
-
-@dp.callback_query(F.data == 'Формулы расчёта')
-async def callback_query_handler(callback: CallbackQuery) -> None:
-
-    await callback.message.answer("10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5")
-    await callback.answer()
+@router.message(lambda message: message.text == 'Рассчитать')
+async def main_menu(message: Message):
+    await message.answer('Выберите опцию:', reply_markup=inline_keyboard)
 
 
-@dp.callback_query(F.data == 'Рассчитать норму калорий')
-async def start_calc(callback: CallbackQuery, state: FSMContext) -> None:
+@router.callback_query(lambda call: call.data == 'formulas')
+async def get_formulas(call):
+    logging.info("Формулы расчета запрошены")
+    await call.message.answer('Формула Миффлина-Сан Жеора: 10 * вес + 6.25 * рост - 5 * возраст + 5 (для мужчин).')
+    await call.answer()
 
+
+@router.callback_query(lambda call: call.data == 'calories')
+async def set_age(call, state: FSMContext):
     await state.set_state(UserState.age)
-    await callback.message.answer("Введите свой возраст:")
-    await callback.answer()
+    await call.message.answer('Введите свой возраст:')
+    await call.answer()
 
 
-@dp.callback_query(F.data == 'product_buying')
-async def buy(callback: CallbackQuery) -> None:
-    await callback.message.answer('Вы успешно приобрели продукт!')
-    await callback.answer()
+@router.message(UserState.age)
+async def set_growth(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await state.set_state(UserState.growth)
+    await message.answer('Введите свой рост:')
+
+@router.message(UserState.growth)
+async def set_weight(message: Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await state.set_state(UserState.weight)
+    await message.answer('Введите свой вес:')
 
 
-# Обработчики состояний
-@dp.message(UserState.age)
-async def set_age(message: Message, state: FSMContext) -> None:
-
-    if message.text.isdigit():
-        await state.update_data(age=message.text)
-        await state.set_state(UserState.growth)
-        await message.answer("Введите свой рост:")
-    else:
-        await message.answer('Введите число, еще раз:')
+@router.message(UserState.weight)
+async def send_calories(message: Message, state: FSMContext):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
 
 
-@dp.message(UserState.growth)
-async def set_growth(message: Message, state: FSMContext) -> None:
-
-    if message.text.isdigit():
-        await state.update_data(growth=message.text)
-        await state.set_state(UserState.weight)
-        await message.answer("Введите свой вес:")
-    else:
-        await message.answer('Введите число, еще раз:')
+    age = int(data.get('age'))
+    growth = int(data.get('growth'))
+    weight = int(data.get('weight'))
 
 
-@dp.message(UserState.weight)
-async def set_weight(message: Message, state: FSMContext) -> None:
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5
 
-    if message.text.isdigit():
-        await state.update_data(weight=message.text)
-        await state.set_state(UserState.growth)
-
-        data = await state.get_data()
-        await state.clear()
-
-        result = float(data["weight"]) * 10 + float(data["growth"]) * 6.25 - float(data["age"]) * 5 + 5
-        await message.answer(f'Ваша норма калорий: {result}')
-    else:
-        await message.answer('Введите число, еще раз:')
+    await message.answer(f'Ваша норма калорий: {calories} ккал в день.')
+    await state.clear()
 
 
-# Обработчик любого текстового сообщения
-@dp.message()
-async def echo_handler(message: Message) -> None:
+async def send_file(message, file_path, file_name=None):
+    try:
+
+        file_input = FSInputFile(file_path, filename=file_name)
+        await bot.send_document(chat_id=message.chat.id, document=file_input)
+        logging.info(f"Файл отправлен: {file_path}")
+    except FileNotFoundError:
+        await message.answer(f"Файл {file_path} не найден.")
+        logging.error(f"Файл не найден: {file_path}")
+    except PermissionError:
+        await message.answer(f"Нет прав доступа к файлу {file_path}.")
+        logging.error(f"Нет прав доступа к файлу: {file_path}")
+    except Exception as e:
+        await message.answer(f"Произошла ошибка при отправке файла: {e}")
+        logging.error(f"Ошибка при отправке файла {file_path}: {e}")
 
 
+@router.message(lambda message: message.text == 'Купить')
+async def get_buying_list(message: Message):
+    image_paths = [
+        'image1.jpeg',
+        'image2.jpeg',
+        'image3.jpeg',
+        'image4.jpeg',
+        'image5.jpeg',
+        'image6.jpeg'
+    ]
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+    for i, image_path in enumerate(image_paths):
+        await message.answer(f'Название: Product{i + 1} | Описание: описание {i + 1} | Цена: {i * 100}')
+        await send_file(message, image_path, file_name=f'Product{i + 1}.jpeg')
+
+    await message.answer('Выберите продукт для покупки:', reply_markup=products_inline_keyboard)
+
+
+@router.callback_query(lambda call: call.data == 'product_buying')
+async def send_confirm_message(call):
+    await call.message.answer('Вы успешно приобрели продукт!')
+    await call.answer()
+
+
+@router.message()
+async def all_messages(message: Message):
+    await message.answer('Пожалуйста, используйте команду /start или нажмите кнопку "Рассчитать", чтобы продолжить.')
+
+
+async def shutdown(dispatcher: Dispatcher):
+    await bot.session.close()
+
+
+async def main():
+    dp.include_router(router)
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await shutdown(dp)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.info("Бот остановлен.")
