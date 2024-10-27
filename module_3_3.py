31b981aedc954fce04ea82019392f029e915261b
sashka199336 <160388443+sashka199336@users.noreply.github.com>
2024-10-27 13:25:49 +1000

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,12 +1,49 @@
-def print_params(a = 1, b = 'строка', c = True):
-    print(a,b,c)
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
 
-print_params(b = 25), print_params(c = [1,2,3])
 
-values_list=(4,'gulash',True)
-values_dict={'a':9,'b':"Hohland", "c":False}
+print_params()  
+print('--------------------------------')
+
+
+print_params(2, 'столбец', False)
+print_params(3, 'None')
+print_params(a='один', b='два', c='три')
+print_params(a='один', b='два')
+print_params(b='два', c='три')
+print_params(a='один', c='три')
+print_params(a='один')
+print_params(b='два')
+print_params(c='три')
+print_params()  
+print('--------------------------------')
+
+
+print_params(b=25)
+print_params(c=[1, 2, 3])
+print('--------------------------------')
+
+
+
+values_list = [9.8, 'const', True]
+
+values_dict = {'a': 'else', 'b': False, 'c': 0}
+
 print_params(*values_list)
 print_params(**values_dict)
+print('--------------------------------')
 
-values_list_2=(14,"guhat")
-print_params(*values_list_2, 52)
+
+
+def append_to_list(item, values_list=None):
+    if values_list is None:
+        values_list = []
+        values_list.append(item)
+
+
+print_params(*values_list)
+print('--------------------------------')
+
+
+values_list_2 = [54.32, 'Строка']  
+print_params(*values_list_2, 42)  
