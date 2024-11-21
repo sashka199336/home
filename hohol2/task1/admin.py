4e47a28f945fc8f5bddaaf7dd2a6204dfa03a7ab
sashka199336 <160388443+sashka199336@users.noreply.github.com>
2024-11-22 05:40:36 +1000

    Add files via upload

---
+++ hohol2/task1/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Buyer, Game
+
+admin.site.register(Buyer)
+admin.site.register(Game)
